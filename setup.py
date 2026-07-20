def56e12d20228bbef645eb0db7d620cb3abb60f
laughingstar101 <shamhyqal@gmail.com>
2026-07-20 11:34:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,6 @@
-msg = "Hello world"
-print(msg)
+from setuptools import setup, find_packages
+
+setup(name='mnist_conv2d_medium_tutorial',
+      packages=find_packages(exclude=['tests']),
+      zip_safe=False,
+      keywords=['convolutional neural network', 'neural network', 'tensorflow', 'mnist'])
