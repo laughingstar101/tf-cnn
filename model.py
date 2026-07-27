7c23e117278f97705bd0c3cecdd51a13078f79fd
laughingstar101 <shamhyqal@gmail.com>
2026-07-27 17:09:02 +0800

    moved data augmentation to the cpu

--- model.py
+++ model.py
@@ -5,22 +5,12 @@ class Model(Model):
     def __init__(self, num_labels=10):
         super(Model, self).__init__()
 
-        self.augment = tf.keras.Sequential([
-            layers.RandomTranslation(height_factor=(-0.1, 0.1), width_factor=(-0.1, 0.1)),
-            layers.RandomRotation(0.2),
-            layers.RandomZoom(height_factor=(-0.1, 0.1)),
-            layers.RandomBrightness(0.2),
-            layers.RandomContrast(0.2),
-            layers.GaussianNoise(stddev=0.1)
-        ])
-
         # Conv layers with ReLU
         self.conv1 = layers.Conv2D(32, (5, 5), padding='same', activation='relu')
         self.pool1 = layers.MaxPooling2D((2, 2))
         self.conv2 = layers.Conv2D(64, (5, 5), padding='same', activation='relu')
         self.pool2 = layers.MaxPooling2D((2, 2))
         self.conv3 = layers.Conv2D(128, (3, 3), padding='same', activation='relu')
-        # No pooling after conv3
         self.flatten = layers.Flatten()
         self.fc1 = layers.Dense(256, activation='relu')
         self.dropout = layers.Dropout(0.2)
@@ -28,8 +18,6 @@ class Model(Model):
 
     def call(self, inputs, training=False):
         x = inputs
-        if training:
-            x = self.augment(x)
         x = self.conv1(x)
         x = self.pool1(x)
         x = self.conv2(x)
--- train.py
+++ train.py
@@ -3,28 +3,45 @@ import tensorflow_addons as tfa
 import mnist
 import argparse
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import time
 from model import Model
 import warnings
 warnings.filterwarnings('ignore')
+tf.get_logger().setLevel('ERROR')
 
 def train(args):
-    # Load data
     images, val_images, labels, val_labels = mnist.load_train_data(args.train_data)
 
-    # Build model
     model = Model(num_labels=10)
 
-    # AdamW optimizer
     optimizer = tfa.optimizers.AdamW(learning_rate=1e-3, weight_decay=1e-4)
-
     model.compile(
         optimizer=optimizer,
         loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
         metrics=['accuracy']
     )
 
-    # Early stopping callback
+    # ---- CPU augmentation pipeline ----
+    train_dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+    train_dataset = train_dataset.shuffle(10000).cache().repeat().batch(args.batch_size)
+
+    augment = tf.keras.Sequential([
+        tf.keras.layers.RandomTranslation(height_factor=(-0.05, 0.05), width_factor=(-0.05, 0.05)),
+        tf.keras.layers.RandomRotation(0.1),
+        tf.keras.layers.RandomZoom(height_factor=(-0.05, 0.05)),
+    ])
+
+    def augment_fn(x, y):
+        x = augment(x, training=True)
+        return x, y
+
+    train_dataset = train_dataset.map(augment_fn, num_parallel_calls=tf.data.AUTOTUNE)
+    train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
+
+    steps_per_epoch = len(images) // args.batch_size
+
+    # Callbacks
     early_stop = tf.keras.callbacks.EarlyStopping(
         monitor='val_accuracy',
         patience=args.patience,
@@ -33,11 +50,13 @@ def train(args):
         verbose=1
     )
 
-    # TensorBoard callback
     log_dir = os.path.join(args.summary_dir, time.strftime("%Y%m%d-%H%M%S"))
-    tensorboard = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    tensorboard = tf.keras.callbacks.TensorBoard(
+        log_dir=log_dir, 
+        histogram_freq=1,
+        profile_batch='2,5'
+    )
 
-    # ModelCheckpoint callback to save best weights
     checkpoint = tf.keras.callbacks.ModelCheckpoint(
         args.checkpoint_file_path + "_best",
         monitor='val_accuracy',
@@ -46,17 +65,15 @@ def train(args):
         verbose=1
     )
 
-    # Train
-    history = model.fit(
-        images, labels,
+    model.fit(
+        train_dataset,
         validation_data=(val_images, val_labels),
-        batch_size=args.batch_size,
         epochs=args.epochs,
+        steps_per_epoch=steps_per_epoch,
         callbacks=[early_stop, tensorboard, checkpoint],
         verbose=1
     )
 
-    # Save final model (weights only)
     model.save_weights(args.checkpoint_file_path)
     print("Training finished.")
 
@@ -64,12 +81,13 @@ if __name__ == '__main__':
     epochs = 100
     patience = 5
     min_delta = 0.0001
+    batch_size = 1024
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', type=int, default=256,
+    parser.add_argument('--batch_size', type=int, default=batch_size,
                         help='size of training batches')
     parser.add_argument('--epochs', type=int, default=epochs,
-                        help='number of training iterations')
+                        help='number of training epochs')
     parser.add_argument('--checkpoint_file_path', type=str,
                         default='checkpoints/model.ckpt',
                         help='path to checkpoint file')
@@ -80,7 +98,7 @@ if __name__ == '__main__':
                         default='graphs',
                         help='path to directory for storing summaries')
     parser.add_argument('--patience', type=int, default=patience,
-                        help='iterations to wait after last improvement before stopping')
+                        help='epochs to wait after last improvement before stopping')
     parser.add_argument('--min_delta', type=float, default=min_delta,
                         help='absolute improvement')
     args = parser.parse_args()
