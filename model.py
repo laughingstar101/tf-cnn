fb2a4273db6231910db8a1ec30323ca20115d7b7
laughingstar101 <shamhyqal@gmail.com>
2026-08-05 16:53:42 +0800

    feat: increase model capacity

--- model.py
+++ model.py
@@ -11,13 +11,20 @@ def create_model(num_labels=10):
         layers.GaussianNoise(stddev=0.1),
         # Convolutional layers
         layers.Conv2D(32, (5, 5), padding='same', activation='relu'),
+        layers.Conv2D(32, (5, 5), padding='same', activation='relu'),
         layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (5, 5), padding='same', activation='relu'),
+        layers.Conv2D(64, (5, 5), padding='same', activation='relu'),
         layers.Conv2D(64, (5, 5), padding='same', activation='relu'),
         layers.MaxPooling2D((2, 2)),
         layers.Conv2D(128, (3, 3), padding='same', activation='relu'),
+        layers.Conv2D(128, (3, 3), padding='same', activation='relu'),
+        layers.Conv2D(128, (3, 3), padding='same', activation='relu'),
+        layers.Conv2D(128, (3, 3), padding='same', activation='relu'),
         layers.GlobalAveragePooling2D(),
         layers.Dense(256, activation='relu'),
-        layers.Dropout(0.2),
+        layers.Dense(256, activation='relu'),
+        layers.Dropout(0.4),
         layers.Dense(num_labels),
     ])
     return model
